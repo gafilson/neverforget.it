4337ba379cde9f428798d0472b222aa9072bc222
Glenn <glenn@ipsy.com>
2016-12-04 21:39:17 -0800

    removing JSONDoc, adding Springfox Swagger implementation

--- nfi-web/src/main/groovy/com/nfi/NfiWebApplication.groovy
+++ nfi-web/src/main/groovy/com/nfi/NfiWebApplication.groovy
@@ -1,14 +1,41 @@
 package com.nfi
 
-import org.jsondoc.spring.boot.starter.EnableJSONDoc
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Import
+import springfox.documentation.builders.ApiInfoBuilder
+import springfox.documentation.service.ApiInfo
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.swagger2.annotations.EnableSwagger2
+
+import static springfox.documentation.builders.PathSelectors.regex
+import springfox.documentation.spring.web.plugins.Docket
 
 @SpringBootApplication
-@EnableJSONDoc
+@EnableSwagger2
+@Import([springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration])
 class NfiWebApplication {
 
 	static void main(String[] args) {
 		SpringApplication.run NfiWebApplication, args
+	}
+
+	@Bean
+	public Docket newsApi() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.groupName("nfi")
+				.apiInfo(apiInfo())
+				.select()
+				.paths(regex("/.*"))
+				.build();
+	}
+
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder()
+				.title("NFI Rest")
+				.description("NFI Rest")
+				.version("0.1")
+				.build();
 	}
 }
