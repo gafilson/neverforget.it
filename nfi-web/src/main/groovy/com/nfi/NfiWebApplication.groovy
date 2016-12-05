fa128b41242701985b1257f431bda38f012f79ae
Glenn <glenn@ipsy.com>
2016-12-04 20:50:02 -0800

    Adding JSONDoc

--- nfi-web/src/main/groovy/com/nfi/NfiWebApplication.groovy
+++ nfi-web/src/main/groovy/com/nfi/NfiWebApplication.groovy
@@ -1,9 +1,11 @@
 package com.nfi
 
+import org.jsondoc.spring.boot.starter.EnableJSONDoc
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication
+@EnableJSONDoc
 class NfiWebApplication {
 
 	static void main(String[] args) {
